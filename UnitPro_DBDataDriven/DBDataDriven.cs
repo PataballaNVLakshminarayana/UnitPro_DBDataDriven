e34f8bd7c882a1b55dbc9e14a99c7d5dd416fff1
PataballaNVLakshminarayana <narayana9573944031@gmail.com>
2026-03-13 16:02:29 +0530

    Failed method added

--- UnitPro_DBDataDriven/DBDataDriven.cs
+++ UnitPro_DBDataDriven/DBDataDriven.cs
@@ -27,12 +27,22 @@ namespace UnitPro_DBDataDriven
             }
         }
         [Test,TestCaseSource(nameof(LoginData))]        
-        public void TC_DBDataDriven(string _username, string _password)
+        public void TC_DBDataDrivenPass(string _username, string _password)
         {
             _driver.FindElement(By.Id("username")).SendKeys(_username);
             _driver.FindElement(By.Id("password")).SendKeys(_password);
             _driver.FindElement(By.Id("login")).Click();
             Assert.That(_driver.Title, Is.EqualTo("Adactin.com - Search Hotel"));
+        }
+        [Test,TestCaseSource(nameof(LoginData))]
+        public void TC_DBDataDrivenFail(string _username, string _password)
+        {
+            _driver.FindElement(By.Id("username")).SendKeys(_username);
+            _driver.FindElement(By.Id("password")).SendKeys(_password);
+            _driver.FindElement(By.Id("login")).Click();
+            string _errorMessage = _driver.FindElement(By.ClassName("auth_error")).Text;
+            //Assert.That(_errorMessage,Is.EqualTo("Invalid Login details or Your Password might have expired. Click here to reset your password"));
+            NUnit.Framework.Assert.That(_driver.FindElement(By.Id("username_span")).Text, Is.EqualTo("Invalid Login details or Your Password might have expired. Click here to reset your password"));
         }
         [TearDown]
         public void DriverClose()
